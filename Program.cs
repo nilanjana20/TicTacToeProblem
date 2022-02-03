03aa5276922b203c1c60371cf0ec8932b5b75f20
nilanjana20 <nilanjanac873@gmail.com>
2022-02-03 10:15:04 +0530

    Nilanjana added UC1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TicTacToeGame
+{
+public class Program
+{
+	public static void Main(string[] args)
+		{
+			TicTacToeGame.TicTacToeGameSolution();
+		}
+}
+}
---
+++ TicTacToeGame.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TicTacToeGame
+{
+   public class TicTacToeGame
+   {
+       static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+       static int player = 1;
+	   public static void TicTacToeGameSolution()
+    {
+        Console.WriteLine("Tic Tac Toe game started !!!");
+    }
+   }
+}
